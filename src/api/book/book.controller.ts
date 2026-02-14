79f0834e69af3a4117a759d6d4546a34d48ff916
AmilaWije <eranga1995@gmail.com>
2026-02-14 23:56:59 +0530

    book create and get all user wise

--- src/api/book/book.controller.ts
+++ src/api/book/book.controller.ts
@@ -15,28 +15,29 @@ import { UpdateBookDto } from './dto/update-book.dto';
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
-  @Post()
+  @Post('create')
   create(@Body() createBookDto: CreateBookDto) {
+    console.log(createBookDto);
     return this.bookService.create(createBookDto);
   }
 
-  @Get()
-  findAll() {
-    return this.bookService.findAll();
-  }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bookService.findOne(+id);
+  findAll(@Param('id') userId: string) {
+    return this.bookService.findAll(+userId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.bookService.update(+id, updateBookDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.bookService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bookService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+  //   return this.bookService.update(+id, updateBookDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.bookService.remove(+id);
+  // }
 }
--- src/api/book/dto/create-book.dto.ts
+++ src/api/book/dto/create-book.dto.ts
@@ -1,1 +1,6 @@
-export class CreateBookDto {}
+export class CreateBookDto {
+    name: string;
+    description: string;
+    summary: string;
+    userId: number;
+}
