f81afa891f762e58dc6e055734057cc5a594d30b
AmilaWije <eranga1995@gmail.com>
2026-02-14 16:53:25 +0530

    registration and feed all user data

--- src/api/user/dto/create-user.dto.ts
+++ src/api/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    f_name: string;
+    l_name: string;
+    username: string;
+    password: string;
+    email: string;
+    country: string;
+    createdAt: string;
+    updatedAt: string;
+}
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,15 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/config/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly DB: PrismaService){}
+
+  async create(userData: CreateUserDto) {
+    try {
+      const newUser = await this.DB.user.create({
+        data: userData,
+        select: {
+          f_name: true,
+          l_name: true,
+          username: true,
+          password: true,
+          email: true,
+          country: true
+        }
+      });
+      return {
+        success: true,
+        message: 'User Registration Successfully',
+        data: newUser
+      }
+    } catch (e) {
+      if(e.code == 'P2002')throw new BadRequestException(`${userData.username} is already in use`);
+      throw new InternalServerErrorException('internal server error');
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const allUsers = await this.DB.user.findMany();
+    return {
+      success: true,
+      message: 'All users Feeded',
+      data: allUsers
+    }
   }
 
   findOne(id: number) {
